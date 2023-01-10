a8d5fab107d309192c7a388dfa5ff3929dea4e6c
RobsonDev <robson.arcoleze29@gmail.com>
2023-01-10 14:54:35 -0300

    Implementado o delete

---
+++ backend/src/main/java/com/devsuperior/bds02/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.devsuperior.bds02.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.bds02.services.exceptions.DataBaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError erro = new StandardError();
+		erro.setTimestamp(Instant.now());
+		erro.setStatus(status.value());
+		erro.setError("Resource not found");
+		erro.setMessage(e.getMessage());
+		erro.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(erro);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError erro = new StandardError();
+		erro.setTimestamp(Instant.now());
+		erro.setStatus(status.value());
+		erro.setError("Data Base exception");
+		erro.setMessage(e.getMessage());
+		erro.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(erro);
+	}
+
+}
